982a739ec9c735f45ff64d1c541b4a15f3ee2251
ascarano <adamscarano@gmail.com>
2014-11-18 13:12:59 -0700

    completed exercise

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,3 +1,10 @@
 class Employment < ActiveRecord::Base
 
+  belongs_to :organization
+  belongs_to :location
+  belongs_to :person
+  belongs_to :job_title
+
+  validates :job_title, :location, :person, :organization, presence: true
+
 end
